8d9e6e5aee0034b88e8f7a4d1b865f0fcb4f5a62
Promise278 <promiseobi2007@gmail.com>
2025-06-18 13:48:37 +0100

    change responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     <div className='bg-yellow-100 h-full md:p-24 p-6'>
         <div className='flex justify-between'>
             <h1 className='text-3xl font-bold'>Desserts</h1>
-            <div className='hidden md:block rounded-md bg-white w-80 h-72 p-4'>
+            <div className='rounded-md bg-white w-80 h-72 p-4'>
               <h2 className='text-red-700 font-bold text-3xl'>Your Cart (0)</h2>
               <div>
                   <img src="/src/assets/illustration-empty-cart.svg" className='ml-24 mt-4 w-36' alt="empty-cart" />
