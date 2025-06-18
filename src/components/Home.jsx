decce44a39ee08705c6b31083f9d0928b722d988
Promise278 <promiseobi2007@gmail.com>
2025-06-18 13:57:46 +0100

    updated responsiveness for Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,8 @@ function Home() {
   return (
     <div className='bg-yellow-100 h-full md:p-24 p-6'>
         <div className='flex justify-between'>
-            <h1 className='text-3xl font-bold md:mb-0 mb-4'>Desserts</h1>
-            <div className='hidden md:block rounded-md bg-white w-80 h-72 p-4'>
+            <h1 className='text-3xl font-bold '>Desserts</h1>
+            <div className='rounded-md bg-white w-80 h-72 p-4'>
               <h2 className='text-red-700 font-bold text-3xl'>Your Cart (0)</h2>
               <div>
                   <img src="/assets/illustration-empty-cart.svg" className='ml-24 mt-4 w-36' alt="empty-cart" />
